91fc534d832d3affce5e9b3e8b62e964c5e3139b
manyshyn <manyshynyuri@gmail.com>
2021-10-05 16:43:35 +0300

    Update repo. Added bulma and js config (#506)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
+// Uncomment eslint-config-react if you want use react without TS
+
 module.exports = {
   extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
-  // extends: '@mate-academy/eslint-config-react',
+  // extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
 };
