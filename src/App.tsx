949a58649504649233e187f265fd83e124ccaf30
manyshyn <manyshynyuri@gmail.com>
2022-04-07 15:17:40 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.scss';
 // import moviesFromServer from './api/movies.json';
 
+// eslint-disable max-len
 export const App: React.FC = () => (
   <div className="page">
     <div className="page-content">
