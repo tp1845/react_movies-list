2f4f7ccd94cad101bb2373e519094277e81354ec
manyshyn <manyshynyuri@gmail.com>
2022-04-07 16:03:18 +0300

    Update and init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
+  rules: {
+    'max-len': ['error', {
+      ignoreTemplateLiterals: true,
+      ignoreComments: true,
+    }],
+    'jsx-a11y/label-has-associated-control': ["error", {
+      assert: "either",
+    }],
+  },
 };
